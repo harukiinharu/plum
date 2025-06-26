12206e31ef3bb26da68c1ff758391f0ce222472a
春木Haruki <harukiinharu@gmail.com>
2025-06-26 22:35:44 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,9 @@
 import { resolve } from 'node:path'
 import { defineConfig } from 'vite'
-import fs from 'fs-extra'
 import IconsResolver from 'unplugin-icons/resolver'
 import Components from 'unplugin-vue-components/vite'
 import Markdown from 'unplugin-vue-markdown/vite'
 import Vue from '@vitejs/plugin-vue'
-import matter from 'gray-matter'
 import AutoImport from 'unplugin-auto-import/vite'
 import UnoCSS from 'unocss/vite'
 import VueRouter from 'unplugin-vue-router/vite'
@@ -29,13 +27,6 @@ export default defineConfig({
       extendRoute(route) {
         const path = route.components.get('default')
         if (!path) return
-
-        if (!path.includes('projects.md') && path.endsWith('.md')) {
-          const { data } = matter(fs.readFileSync(path, 'utf-8'))
-          route.addToMeta({
-            frontmatter: data,
-          })
-        }
       },
     }),
 
