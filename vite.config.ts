47d77b2d7d1172337b2d8f2f048e9acfdc008860
春木Haruki <harukiinharu@gmail.com>
2025-06-26 22:16:23 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,44 +1,23 @@
 import { resolve } from 'node:path'
 import { defineConfig } from 'vite'
 import fs from 'fs-extra'
-import Inspect from 'vite-plugin-inspect'
-import Icons from 'unplugin-icons/vite'
 import IconsResolver from 'unplugin-icons/resolver'
 import Components from 'unplugin-vue-components/vite'
 import Markdown from 'unplugin-vue-markdown/vite'
 import Vue from '@vitejs/plugin-vue'
 import matter from 'gray-matter'
 import AutoImport from 'unplugin-auto-import/vite'
-import anchor from 'markdown-it-anchor'
-import LinkAttributes from 'markdown-it-link-attributes'
-import MarkdownItGitHubAlerts from 'markdown-it-github-alerts'
 import UnoCSS from 'unocss/vite'
-import SVG from 'vite-svg-loader'
-import MarkdownItShiki from '@shikijs/markdown-it'
-import { rendererRich, transformerTwoslash } from '@shikijs/twoslash'
-import MarkdownItMagicLink from 'markdown-it-magic-link'
 import VueRouter from 'unplugin-vue-router/vite'
 import { VueRouterAutoImports } from 'unplugin-vue-router'
 
-// @ts-expect-error missing types
-import TOC from 'markdown-it-table-of-contents'
-
-const promises: Promise<any>[] = []
-
 export default defineConfig({
+  base: './',
   resolve: {
-    alias: [
-      { find: '~/', replacement: `${resolve(__dirname, 'src')}/` },
-    ],
+    alias: [{ find: '~/', replacement: `${resolve(__dirname, 'src')}/` }],
   },
   optimizeDeps: {
-    include: [
-      'vue',
-      'vue-router',
-      '@vueuse/core',
-      'dayjs',
-      'dayjs/plugin/localizedFormat',
-    ],
+    include: ['vue', 'vue-router', '@vueuse/core', 'dayjs', 'dayjs/plugin/localizedFormat'],
   },
   plugins: [
     UnoCSS(),
@@ -49,8 +28,7 @@ export default defineConfig({
       logs: true,
       extendRoute(route) {
         const path = route.components.get('default')
-        if (!path)
-          return
+        if (!path) return
 
         if (!path.includes('projects.md') && path.endsWith('.md')) {
           const { data } = matter(fs.readFileSync(path, 'utf-8'))
@@ -69,12 +47,8 @@ export default defineConfig({
     }),
 
     Markdown({
-      wrapperComponent: id => id.includes('/demo/')
-        ? 'WrapperDemo'
-        : id.includes('/lyric/') ? 'WrapperLyric' : 'WrapperPost',
-      wrapperClasses: (id, code) => code.includes('@layout-full-width')
-        ? ''
-        : 'prose m-auto slide-enter-content',
+      wrapperComponent: id => 'WrapperPost',
+      wrapperClasses: (id, code) => (code.includes('@layout-full-width') ? '' : 'prose m-auto slide-enter-content'),
       headEnabled: true,
       exportFrontmatter: false,
       exposeFrontmatter: false,
@@ -85,11 +59,7 @@ export default defineConfig({
     }),
 
     AutoImport({
-      imports: [
-        'vue',
-        VueRouterAutoImports,
-        '@vueuse/core',
-      ],
+      imports: ['vue', VueRouterAutoImports, '@vueuse/core'],
     }),
 
     Components({
@@ -102,32 +72,12 @@ export default defineConfig({
         }),
       ],
     }),
-
-    Inspect(),
-
-    Icons({
-      defaultClass: 'inline',
-      defaultStyle: 'vertical-align: sub;',
-    }),
-
-    SVG({
-      svgo: false,
-      defaultImport: 'url',
-    }),
-
-    {
-      name: 'await',
-      async closeBundle() {
-        await Promise.all(promises)
-      },
-    },
   ],
 
   build: {
     rollupOptions: {
       onwarn(warning, next) {
-        if (warning.code !== 'UNUSED_EXTERNAL_IMPORT')
-          next(warning)
+        if (warning.code !== 'UNUSED_EXTERNAL_IMPORT') next(warning)
       },
     },
   },
